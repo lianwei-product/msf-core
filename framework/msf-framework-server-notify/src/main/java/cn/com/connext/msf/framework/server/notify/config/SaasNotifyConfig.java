ed1e86d2aea65eaabd7b81940d9b5f77c908c188
yanzx <505938697@qq.com>
2020-12-29 12:20:19 +0800

    fix

--- framework/msf-framework-server-notify/src/main/java/cn/com/connext/msf/framework/server/notify/config/SaasNotifyConfig.java
+++ framework/msf-framework-server-notify/src/main/java/cn/com/connext/msf/framework/server/notify/config/SaasNotifyConfig.java
@@ -1,10 +1,15 @@
 package cn.com.connext.msf.framework.server.notify.config;
 
+import cn.com.connext.msf.framework.server.notify.domain.LocalNotifyManager;
 import cn.com.connext.msf.framework.server.notify.domain.NotifyManager;
+import cn.com.connext.msf.framework.server.notify.processor.NotificationSendEventProcessor;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
+
+import java.util.List;
 
 @Configuration
 @ComponentScan({
@@ -17,6 +22,12 @@ public class SaasNotifyConfig {
 
     public SaasNotifyConfig() {
         LoggerFactory.getLogger(SaasNotifyConfig.class).info("Init uaa changed mq model");
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(NotifyManager.class)
+    public NotifyManager notifyManager(List<NotificationSendEventProcessor> processChain) {
+        return new LocalNotifyManager(processChain);
     }
 
 }
--- framework/msf-framework-server-notify/src/main/java/cn/com/connext/msf/framework/server/notify/domain/LocalNotifyManager.java
+++ framework/msf-framework-server-notify/src/main/java/cn/com/connext/msf/framework/server/notify/domain/LocalNotifyManager.java
@@ -3,12 +3,14 @@ package cn.com.connext.msf.framework.server.notify.domain;
 import cn.com.connext.msf.framework.server.notify.event.SaasNotificationEvent;
 import cn.com.connext.msf.framework.server.notify.processor.NotificationSendEventProcessor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.messaging.MessageChannel;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
-@ConditionalOnClass(NotifyManager.class)
-@Component
+
 public class LocalNotifyManager implements NotifyManager {
     private final List<NotificationSendEventProcessor> processChain;
 
